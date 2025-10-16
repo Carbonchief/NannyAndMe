a90c9dbacf5531064ca70383ba26f66ed4eba271
Luan van der Walt <30735486+Carbonchief@users.noreply.github.com>
2025-10-16 16:01:05 +0200

    Use logo asset in splash screen

--- babynanny/Views/SplashScreenView.swift
+++ babynanny/Views/SplashScreenView.swift
@@ -3,14 +3,15 @@ import UIKit
 
 struct SplashScreenView: View {
     private let iconSize: CGFloat = 160
+    private let logoAssetName = "Logo"
 
     var body: some View {
         ZStack {
             Color(.systemBackground)
                 .ignoresSafeArea()
 
-            if let icon = UIImage.appIcon {
-                Image(uiImage: icon)
+            if let logo = UIImage(named: logoAssetName) {
+                Image(uiImage: logo)
                     .resizable()
                     .scaledToFit()
                     .frame(width: iconSize, height: iconSize)
