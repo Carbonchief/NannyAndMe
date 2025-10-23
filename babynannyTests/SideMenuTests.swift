a90c0cc357968852cf40ab2ab589af1919c216c0
Luan van der Walt <30735486+Carbonchief@users.noreply.github.com>
2025-10-23 23:20:53 +0200

    Add share profile entry to the side menu

--- babynannyTests/SideMenuTests.swift
+++ babynannyTests/SideMenuTests.swift
@@ -16,6 +16,19 @@ final class SideMenuTests: XCTestCase {
         let labels = hosting.view.allLabelTexts()
         XCTAssertFalse(labels.contains(L10n.Menu.shareProfile))
     }
+
+    func testShareButtonVisibleWhenCloudAvailable() {
+        let view = SideMenu(isCloudSharingAvailable: true,
+                            onSelectAllLogs: {},
+                            onSelectShareProfile: {},
+                            onSelectSettings: {},
+                            onSelectShareData: {})
+        let hosting = UIHostingController(rootView: view)
+        hosting.view.frame = CGRect(x: 0, y: 0, width: 320, height: 640)
+        hosting.view.layoutIfNeeded()
+        let labels = hosting.view.allLabelTexts()
+        XCTAssertTrue(labels.contains(L10n.Menu.shareProfile))
+    }
 }
 
 private extension UIView {
